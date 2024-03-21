2698c99cb782551cd3b7f6ba0fc6973dfb37e392
leoboyerbx <leo.boyer@outlook.com>
2024-03-21 11:42:06 +0100

    Update date

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
-const date = new Date('2024-02-14 12:00:00')
+const date = new Date('2024-03-21 11:00:00')
 const today = new Date();
 
 let diff = today.getTime() - date.getTime();
 const days = Math.round(diff / (1000 * 3600 * 24));
 document.title = days + ' days since the last time Duli rickrolled me'
 
-const highScore = 0
+const highScore = 36
 </script>
 
 <template>
